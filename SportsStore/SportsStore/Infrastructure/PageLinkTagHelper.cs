1316573ce857a37ded83c32aed6ae32ed9a7b296
Rafal <klimek1008@gmail.com>
2024-03-25 00:19:48 +0100

    Added bootstrap

--- SportsStore/SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsStore/SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -25,6 +25,11 @@ namespace SportsStore.Infrastructure
 
         public string? PageAction { get; set; }
 
+        public bool PageClassEnabled { get; set; } = false;
+        public string PageClass { get; set; } = string.Empty;
+        public string PageClassNormal { get; set; } = string.Empty;
+        public string PageClassSelected { get; set; } = string.Empty;
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if (ViewContext != null && PageModel != null)
@@ -36,6 +41,11 @@ namespace SportsStore.Infrastructure
                 {
                     TagBuilder tag = new TagBuilder("a");
                     tag.Attributes["href"] = urlHelper.Action(PageAction, new { productPage = i });
+                    if (PageClassEnabled)
+                    {
+                        tag.AddCssClass(PageClass);
+                        tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                    }
                     tag.InnerHtml.Append(i.ToString());
                     result.InnerHtml.AppendHtml(tag);
                 }
