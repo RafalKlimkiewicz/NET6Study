b0415821a52e42ca51001fa2e5238164d5f3e454
Rafal <klimek1008@gmail.com>
2024-03-31 16:32:07 +0200

    Added Category tests

--- SportsStore/Tests/SportsStore.Tests/HomeControllerTests.cs
+++ SportsStore/Tests/SportsStore.Tests/HomeControllerTests.cs
@@ -95,7 +95,6 @@ namespace SportsStore.Tests
         public void Can_Filter_Products()
         {
             //Arrange
-
             Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
 
             mock.Setup(m => m.Products).Returns((new Product[]
@@ -106,7 +105,6 @@ namespace SportsStore.Tests
                 new Product {ProductID = 4, Name = "P4", Category = "Cat2"},
                 new Product {ProductID = 5, Name = "P5", Category = "Cat3"},
             }).AsQueryable());
-
 
             HomeController controller = new HomeController(mock.Object);
             controller.PageSize = 3;
@@ -119,6 +117,40 @@ namespace SportsStore.Tests
             Assert.Equal(2, result.Length);
             Assert.True(result[0].Name == "P2" && result[0].Category == "Cat2");
             Assert.True(result[1].Name == "P4" && result[1].Category == "Cat2");
+        }
+
+        [Fact]
+        public void Generate_Category_Specific_Product_Count()
+        {
+            //Arrange
+            Mock<IStoreRepository> mock = new();
+
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category = "Cat1"},
+                new Product {ProductID = 2, Name = "P2", Category = "Cat2"},
+                new Product {ProductID = 3, Name = "P3", Category = "Cat1"},
+                new Product {ProductID = 4, Name = "P4", Category = "Cat2"},
+                new Product {ProductID = 5, Name = "P5", Category = "Cat3"},
+            }).AsQueryable());
+
+
+            HomeController target = new HomeController(mock.Object);
+            target.PageSize = 3;
+
+            Func<ViewResult, ProductsListViewModel?> GetModel = result => result?.ViewData?.Model as ProductsListViewModel;
+
+            //Action 
+            var res1 = GetModel(target.Index("Cat1"))?.PagingInfo.TotalItems;
+            var res2 = GetModel(target.Index("Cat2"))?.PagingInfo.TotalItems;
+            var res3 = GetModel(target.Index("Cat3"))?.PagingInfo.TotalItems;
+            var resAll = GetModel(target.Index(null))?.PagingInfo.TotalItems;
+
+            //Assert
+            Assert.Equal(2, res1);
+            Assert.Equal(2, res2);
+            Assert.Equal(1, res3);
+            Assert.Equal(5, resAll);
         }
     }
 }
