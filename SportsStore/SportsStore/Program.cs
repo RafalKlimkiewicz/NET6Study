66418221e1ae9d8654f3e84bb3e4f6eca283bbab
Rafal <klimek1008@gmail.com>
2024-04-01 15:03:36 +0200

    CartTests, Cart View, RazorPages

--- SportsStore/SportsStore/Program.cs
+++ SportsStore/SportsStore/Program.cs
@@ -4,18 +4,24 @@ using SportsStore.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
 builder.Services.AddDbContext<StoreDbContext>(opts =>
 {
     opts.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
 });
 
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>(); ;
+builder.Services.AddRazorPages();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
 
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
 
 app.UseStaticFiles();
+
+app.UseSession();
 
 app.MapControllerRoute("catpage", "{category}/Page{productPage:int}", new { Controller = "Home", action = "Index" });
 
@@ -26,6 +32,8 @@ app.MapControllerRoute("category", "category/{category}", new { Controller = "Ho
 app.MapControllerRoute("pagination", "Products/Page{productPage}", new { Controller = "Home", action = "Index", productPage = 1 });
 
 app.MapDefaultControllerRoute();
+
+app.MapRazorPages();
 
 SeedData.EnsurePopulated(app);
 
