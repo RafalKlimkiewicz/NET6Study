9641255c295f6a068528cb27035ab69d00721f63
Rafal <klimek1008@gmail.com>
2024-06-18 08:04:37 +0200

    Checkout

--- SportsStore/SportsStore/Controllers/OrderController.cs
+++ SportsStore/SportsStore/Controllers/OrderController.cs
@@ -6,10 +6,34 @@ namespace SportsStore.Controllers
     public class OrderController : Controller
     {
         private IOrderRepository _orderRepository;
-        private Cart cart;
+        private Cart _cart;
 
-        public OrderController() { }
+        public OrderController(IOrderRepository orderRepository, Cart cart)
+        {
+            _orderRepository = orderRepository;
+            _cart = cart;
+        }
 
         public ViewResult Checkout() => View(new Order());
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (_cart.Lines.Count() == 0)
+                ModelState.AddModelError("", "Sorry, your cart is empty");
+
+            if (ModelState.IsValid)
+            {
+                order.Lines = _cart.Lines.ToArray();
+
+                _orderRepository.SaveOrder(order);
+
+                _cart.Clear();
+
+                return RedirectToPage("/Completed", new { orderId = order.OrderID });
+            }
+
+            return View();
+        }
     }
 }
---
+++ SportsStore/Tests/SportsStore.Tests/OrderControllerTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+
+namespace SportsStore.Tests
+{
+    public class OrderControllerTests
+    {
+        [Fact]
+        public void Cannot_Checkout_Empty_Cart()
+        {
+            //Arrange
+            var mock = new Mock<IOrderRepository>();
+
+            var cart = new Cart();
+
+            var order = new Order();
+
+            OrderController target = new OrderController(mock.Object, cart);
+
+            //Act
+            ViewResult? result = target.Checkout(order) as ViewResult;
+
+            mock.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Never);
+
+            Assert.True(string.IsNullOrEmpty(result?.ViewName));
+
+            Assert.False(result?.ViewData.ModelState.IsValid);
+        }
+
+        [Fact]
+        public void Cannot_Checokut_Invalid_ShippingDetails()
+        {
+            //Arrange
+            var mock = new Mock<IOrderRepository>();
+
+            var cart = new Cart();
+
+            cart.AddItem(new Product(), 1);
+
+            var target = new OrderController(mock.Object, cart);
+
+            target.ModelState.AddModelError("error", "error");
+
+            //Act
+            ViewResult? result = target.Checkout(new Order()) as ViewResult;
+
+            mock.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Never());
+
+            Assert.True(string.IsNullOrEmpty(result?.ViewName));
+
+            Assert.False(result?.ViewData.ModelState.IsValid);
+        }
+
+        [Fact]
+        public void Can_Checkout_And_Sumbit_Order()
+        {
+            //Arrange
+            Mock<IOrderRepository> mock = new Mock<IOrderRepository>();
+
+            var cart = new Cart();
+
+            cart.AddItem(new Product(), 1);
+
+            //Act
+            var target = new OrderController(mock.Object, cart);
+
+            var result = target.Checkout(new Order()) as RedirectToPageResult;
+
+            mock.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Once);
+
+            Assert.Equal("/Completed", result?.PageName);
+        }
+    }
+}
