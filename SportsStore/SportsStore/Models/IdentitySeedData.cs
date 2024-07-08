150169e4beaa23f19abfd48ed58b139e58071211
Rafal <klimek1008@gmail.com>
2024-07-08 21:04:05 +0200

    Added authorization admin part 1

---
+++ SportsStore/SportsStore/Models/IdentitySeedData.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Models
+{
+    public static class IdentitySeedData
+    {
+        private const string adminUser = "Admin";
+        private const string adminPassword = "Secret123$";
+
+        public static async void EnsurePopulated(IApplicationBuilder app)
+        {
+            var context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<AppIdentityDbContext>();
+
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+
+            UserManager<IdentityUser> userManager = app.ApplicationServices.CreateScope().ServiceProvider.
+                GetRequiredService<UserManager<IdentityUser>>();
+
+            var user = await userManager.FindByNameAsync(adminUser);
+            if (user == null)
+            {
+                user = new IdentityUser("Admin");
+                user.Email = "admin@example.com";
+                user.PhoneNumber = "012-345-679";
+
+                await userManager.CreateAsync(user, adminPassword);
+            }
+        }
+    }
+}
