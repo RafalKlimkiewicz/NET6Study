779e4a3320aac7aabff7afd8c0212c2dd39b2e01
Rafal <klimek1008@gmail.com>
2024-07-18 23:28:02 +0200

    Added Docker run, login page

---
+++ SportsStore/SportsStore/Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace SportsStore.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Password { get; set; }
+        public string ReturnUrl { get; set; } = "/";
+
+    }
+}
