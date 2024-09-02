f68993cc48fb25f69d13fbb8ce1a60544bc3f5b5
Rafal <klimek1008@gmail.com>
2024-09-02 20:05:30 +0200

    Last part of routing, ambigous, custom constrains, Order,  dispalyname

--- CorePlatform/Platform/Program.cs
+++ CorePlatform/Platform/Program.cs
@@ -1,4 +1,3 @@
-using Microsoft.Extensions.Options;
 using Platform;
 using Platform.Middlewares;
 
@@ -9,13 +8,50 @@ builder.Services.Configure<MessageOption>(options =>
     options.CityName = "Albany";
 });
 
+builder.Services.Configure<RouteOptions>(opts =>
+{
+    opts.ConstraintMap.Add("countryName", typeof(CountryRouteConstraint));
+});
+
+
 var app = builder.Build();
 
-//app.UseMiddleware<LocationMiddleware>();
+
+app.Use(async (context, next) =>
+{
+    Endpoint? end = context.GetEndpoint();
+
+    if(end != null)
+        await context.Response.WriteAsync($"{end.DisplayName} Selected");
+    else
+        await context.Response.WriteAsync($"No endpoint Selected");
+
+    await next();
+});
+
+
+
+//ambigous route  -- fix Add Route Endpoint Builder order
+app.Map("{number:int}", async context =>
+{
+    await context.Response.WriteAsync("Routed to the int endpoint");
+}).WithDisplayName("INT endpoint")
+.Add(b => ((RouteEndpointBuilder)b).Order = 1); //fix
+
+app.Map("{number:double}", async context =>
+{
+    await context.Response.WriteAsync("Routed to the dobule endpoint");
+}).WithDisplayName("Double endpoint")
+.Add(b => ((RouteEndpointBuilder)b).Order = 2); //fix
+
+//---
+
+app.UseMiddleware<LocationMiddleware>();
 //app.UseMiddleware<Population>();
 //app.UseMiddleware<Capital>();
 
-app.MapGet("files/{filename}.{ext}", async context =>
+//alpha - from a to -z
+app.MapGet("{first:alpha:length(3)}/{second:bool}/{*catchcall}", async context =>
 {
     await context.Response.WriteAsync("Request was Routed\n");
 
@@ -23,7 +59,16 @@ app.MapGet("files/{filename}.{ext}", async context =>
         await context.Response.WriteAsync($"{kvp.Key}: {kvp.Value}\n");
 });
 
-app.MapGet("{first}/{second}/{third}", async context =>
+app.MapGet("{first}/{second}/{*catchcall}", async context =>
+{
+    await context.Response.WriteAsync("Request was Routed\n");
+
+    foreach (var kvp in context.Request.RouteValues)
+        await context.Response.WriteAsync($"{kvp.Key}: {kvp.Value}\n");
+});
+
+
+app.MapGet("{first:int}/{second:bool}", async context =>
 {
     await context.Response.WriteAsync("Request was Routed\n");
 
@@ -31,7 +76,11 @@ app.MapGet("{first}/{second}/{third}", async context =>
         await context.Response.WriteAsync($"{route.Key}: {route.Value}\n");
 });
 
+app.MapGet("capital2/{country:countryName}", Capital.Endpoint);
+
 app.MapGet("capital/{country=France}", Capital.Endpoint);
+
+app.MapGet("capital/{country:regex(^uk|france|monaco$)}", Capital.Endpoint);
 app.MapGet("size/{city?}", Population.Endpoint)
     .WithMetadata(new RouteNameMetadata("population"));
 
@@ -117,5 +166,10 @@ app.MapGet("size/{city?}", Population.Endpoint)
 //app.UseMiddleware<QueryStringMiddleWare>();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapFallback(async context =>
+{
+    await context.Response.WriteAsync("Routed to fallback endpoint");
+});
 
 app.Run();
