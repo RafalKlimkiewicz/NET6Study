0c98259fec8f4f1fcaa0fbb99ab1a3b7efde8a05
Rafal <klimek1008@gmail.com>
2024-09-04 20:23:10 +0200

    DI singleton

--- CorePlatform/Platform/Program.cs
+++ CorePlatform/Platform/Program.cs
@@ -1,28 +1,28 @@
+using System.Runtime.Serialization;
 using Platform.Classes;
 using Platform.Middlewares;
 using Platform.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton<IResponseFormatter, HtmlResponseFormatter>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-
 app.UseMiddleware<WeatherMiddleware>();
 
-//IResponseFormatter formatter = new TextResponseFormatter();
-
-app.MapGet("middleware/function", async (context) =>
+app.MapGet("middleware/function", async (HttpContext context, HtmlResponseFormatter formatter) =>
 {
-    await TypeBroker.Formatter.Format(context, "Middleware Function: It is snowing in Chicago");
+    await formatter.Format(context, "Middleware Function: It is snowing in Chicago");
 });
 
 app.MapGet("endpoint/class", WeatherEndpoint.Endpoint);
 
-app.MapGet("endpoint/function", async (context) =>
+app.MapGet("endpoint/function", async (HttpContext context, HtmlResponseFormatter formatter) =>
 {
-    await TypeBroker.Formatter.Format(context, "Endpoint Function: It is sunny in LA");
+    await formatter.Format(context, "Endpoint Function: It is sunny in LA");
 });
 
 app.MapFallback(async context =>
