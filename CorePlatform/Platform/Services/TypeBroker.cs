5766ca7f5516ba4c44db783a98c748e751cfb582
Rafal <klimek1008@gmail.com>
2024-09-04 20:15:54 +0200

    TypeBroker - singleton but with replaceable implementation

--- CorePlatform/Platform/Services/TypeBroker.cs
+++ CorePlatform/Platform/Services/TypeBroker.cs
@@ -2,7 +2,8 @@
 {
     public static class TypeBroker
     {
-        private static IResponseFormatter formatter = new TextResponseFormatter();
+        private static IResponseFormatter formatter = new HtmlResponseFormatter();
+        //private static IResponseFormatter formatter = new TextResponseFormatter();
 
         public static IResponseFormatter Formatter => formatter;
     }
