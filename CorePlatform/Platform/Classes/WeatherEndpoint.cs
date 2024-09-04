bc53767e9eb5ef97d4989515e63ed94d31c07ea1
Rafal <klimek1008@gmail.com>
2024-09-04 20:59:58 +0200

    DI| Service as endpoint

--- CorePlatform/Platform/Classes/WeatherEndpoint.cs
+++ CorePlatform/Platform/Classes/WeatherEndpoint.cs
@@ -1,10 +1,16 @@
-﻿namespace Platform.Classes
+﻿using Platform.Services;
+
+namespace Platform.Classes
 {
     public class WeatherEndpoint
     {
         public static async Task Endpoint(HttpContext context)
         {
-            await context.Response.WriteAsync("Endpoint Class: It is couldy in Milan");
+            var formatter = context.RequestServices.GetRequiredService<IResponseFormatter>();
+
+            await formatter.Format(context, "Endpoint Class: It is couldy in Milan");
+
+            //await context.Response.WriteAsync("Endpoint Class: It is couldy in Milan");
         }
     }
 }
