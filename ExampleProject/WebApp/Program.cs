2a8aaf7d481c3e84d2f37d3faadfea75908896a9
Rafal K. <klimek1008@gmail.com>
2024-12-10 21:48:26 +0100

    Init Web Project

---
+++ ExampleProject/WebApp/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models.DB;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:ProductConnection"]);
+    opts.EnableSensitiveDataLogging(true);
+});
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
+
+SeedData.SeedDatabase(context);
+
+app.Run();
