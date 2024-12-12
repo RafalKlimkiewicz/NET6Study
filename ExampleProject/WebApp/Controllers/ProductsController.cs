8f5e25d5c7a974c86da74cd6920fcfa2b10c48d5
Rafal K. <klimek1008@gmail.com>
2024-12-12 22:31:42 +0100

    Controller example

---
+++ ExampleProject/WebApp/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+using WebApp.Models.DB;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private DataContext _context;
+
+        public ProductsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return new[]
+            {
+                new Product
+                {
+                    Name = "Product #1",
+                },
+                new Product
+                {
+                    Name = "Product #2",
+                }
+
+            };
+        }
+
+        [HttpGet("{id}")]
+        public Product GetProduct(long id)
+        {
+            return new Product()
+            {
+                ProductId = id,
+                Name = "Test Product",
+            };
+        }
+    }
+}
