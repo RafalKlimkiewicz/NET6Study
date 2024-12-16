32447904e0305b5d19071307d4e3580180c56b62
Rafal K. <klimek1008@gmail.com>
2024-12-16 19:17:09 +0100

    Update & delete

--- ExampleProject/WebApp/Controllers/ProductsController.cs
+++ ExampleProject/WebApp/Controllers/ProductsController.cs
@@ -27,5 +27,29 @@ namespace WebApp.Controllers
 
             return _context.Products.Find(id);
         }
+
+        [HttpPost]
+        public void SaveProduct([FromBody] Product product)
+        {
+            _context.Products.Add(product);
+
+            _context.SaveChanges();
+        }
+
+        [HttpPut]
+        public void UpdateProduct([FromBody] Product product)
+        {
+            _context.Products.Update(product);
+
+            _context.SaveChanges();
+        }
+
+        [HttpDelete("id")]
+        public void DeleteProduct([FromBody] long id)
+        {
+            _context.Products.Remove(new Product { ProductId = id });
+
+            _context.SaveChanges();
+        }
     }
 }
