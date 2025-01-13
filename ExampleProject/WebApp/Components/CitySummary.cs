3af09569691b989550418610253649421e9ea97c
Rafal K. <klimek1008@gmail.com>
2025-01-13 21:20:17 +0100

    ViewComponents

---
+++ ExampleProject/WebApp/Components/CitySummary.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models.DB;
+
+namespace WebApp.Components
+{
+    public class CitySummary : ViewComponent
+    {
+        private CitiesData _citiesData;
+
+        public CitySummary(CitiesData cdata)
+        {
+            _citiesData = cdata;
+        }
+
+        public string Invoke()
+        {
+            return $"{_citiesData.Cities.Count()} cities, {_citiesData.Cities.Sum(c => c.Population)} people";
+        }
+    }
+}
