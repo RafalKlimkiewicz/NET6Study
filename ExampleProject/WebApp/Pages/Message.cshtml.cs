6cf98c959365909f39fa9e59977c9bac3df0bd30
Rafal K. <klimek1008@gmail.com>
2025-02-05 21:57:12 +0100

    Action & Page Filters

--- ExampleProject/WebApp/Pages/Message.cshtml.cs
+++ ExampleProject/WebApp/Pages/Message.cshtml.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebApp.Filters;
 
@@ -7,13 +8,23 @@ namespace WebApp.Pages
     [RequireHttps]
     //[HttpsOnly]
     //[SimpleCache]
-    [SimpleCacheAsync]
+    //[SimpleCacheAsync]
+    [ChangePageArgs]
     public class MessageModel : PageModel
     {
         public object Message { get; set; } = $"{DateTime.Now.ToLongTimeString()} This is the Message Razor Page";
 
-        public void OnGet()
+        public void OnGet(string message1, string message2)
         {
+            Message = $"{message1}, {message2}";
         }
+
+        //public override void OnPageHandlerExecuting(PageHandlerExecutingContext context)
+        //{
+        //    if (context.HandlerArguments.ContainsKey("message1"))
+        //    {
+        //        context.HandlerArguments["message1"] = "New message";
+        //    }
+        //}
     }
 }
