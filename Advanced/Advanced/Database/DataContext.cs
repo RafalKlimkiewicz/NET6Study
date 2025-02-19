568218b53d1082bf75ee91081d75591e768b86b0
Rafal K. <klimek1008@gmail.com>
2025-02-20 00:25:11 +0100

    init Advanced project

---
+++ Advanced/Advanced/Database/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Advanced.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Advanced.Database
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> opts)    : base(opts) { }
+
+        public DbSet<Person> People => Set<Person>();
+        public DbSet<Department> Departments => Set<Department>();
+        public DbSet<Location> Locations => Set<Location>();
+    }
+}
---
+++ Advanced/Advanced/Program.cs
@@ -0,0 +1,22 @@
+using Advanced.Database;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:PeopleConnection"]);
+    opts.EnableSensitiveDataLogging(true);
+});
+
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+
+var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
+
+SeedData.SeedDatabase(context);
+
+app.Run();
