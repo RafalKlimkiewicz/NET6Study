1893a8a92791a04e93f821b2ec0d794dd3e51604
Rafal K. <klimek1008@gmail.com>
2025-02-26 22:52:42 +0100

    blazor server

--- Advanced/Advanced/ViewModels/PeopleListViewModel.cs
+++ Advanced/Advanced/ViewModels/PeopleListViewModel.cs
@@ -8,6 +8,6 @@ namespace Advanced.ViewModels
         public IEnumerable<string> Cities { get; set; } = Enumerable.Empty<string>();
         public string SelectedCity { get; set; } = string.Empty;
 
-        public string GetClass(string? city) => SelectedCity == city ? "bg-info text-white" : "";
+        public string GetClass(string? city) => SelectedCity == city ? "--bs-table-bg: transparent; --bs-table-bg-state: transparent; background-color: #17a2b8 !important; color: #fff !important; font-weight: bold;" : "";
     }
 }
